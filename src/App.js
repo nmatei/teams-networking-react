2e2a28498c96cea524777841b136c135e89c8a5b
Nicolae Matei <nmatei@sdl.com>
2021-03-15 21:40:21 +0200

    add TeamsTable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { TeamsTable } from "./TeamsTable";
+
+let teams = [
+  {
+    "name": "CV",
+    "members": "Nicolae Matei",
+    "url": "https://github.com/nmatei/nmatei2.github.io"
+  },
+  {
+    "name": "teams-networking",
+    "members": "Nicolae Matei",
+    "url": "https://github.com/nmatei/teams-networking/tree/won2"
+  }
+];
 
 function App() {
   return (
     <div>
       <h1>Teams Networking</h1>
       <div>Search</div>
-      <p>table...</p>
+      <TeamsTable teams={teams} />
     </div>
   );
 }
