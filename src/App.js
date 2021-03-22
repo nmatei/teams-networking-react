c17b1353e30e3070be5e7193aa9b16f08e4f2f64
Nicolae Matei <nmatei@sdl.com>
2021-03-22 20:10:17 +0200

    add Component as class + internal state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
+import { Component } from 'react';
 import './App.css';
 import { TeamsTable } from "./TeamsTable";
 
-let teams = [
-  {
-    "name": "CV",
-    "members": "Nicolae Matei",
-    "url": "https://github.com/nmatei/nmatei2.github.io"
-  },
-  {
-    "name": "teams-networking",
-    "members": "Nicolae Matei",
-    "url": "https://github.com/nmatei/teams-networking/tree/won2"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: [],
+      date: new Date().toString()
+    }
   }
-];
 
-function App() {
-  return (
-    <div>
-      <h1>Teams Networking</h1>
-      <div>Search</div>
-      <TeamsTable teams={teams} border={1} />
-    </div>
-  );
+  componentDidMount() {
+    console.warn('mount');
+
+    setInterval(() => {
+      this.setState({
+        date: new Date().toString()
+      })
+    }, 60000);
+
+    setTimeout(() => {
+      console.warn("loaded");
+      this.setState({
+        teams: [
+          {
+            "name": "CV",
+            "members": "Nicolae Matei",
+            "url": "https://github.com/nmatei/nmatei2.github.io"
+          },
+          {
+            "name": "teams-networking",
+            "members": "Nicolae Matei",
+            "url": "https://github.com/nmatei/teams-networking/tree/won2"
+          }
+        ]
+      })
+    }, 2000);
+  }
+  
+  render() {
+    console.debug(this.state.teams);
+    return (
+      <div>
+        <h1>Teams Networking</h1>
+        <div>Search</div>
+        <TeamsTable teams={this.state.teams} border={1} />
+        <div>{this.state.date}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
