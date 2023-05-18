79fde8ac2a46237aefd811cec4ac8a7f82fa08e9
Matei Nicolae <nmatei@sdl.com>
2023-05-18 21:09:34 +0300

    add MainMenu function component

--- src/App.tsx
+++ src/App.tsx
@@ -15,30 +15,35 @@ function AppHeader() {
           <h2 id="job-title">Trainer @ FastTrackIT & Udemy</h2>
         </div>
       </div>
-
-      <ul id="top-menu-bar">
-        <li>
-          <a href="#" data-page="home">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="skills">
-            Skills
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="projects">
-            Projects
-          </a>
-        </li>
-        <li>
-          <a href="#" data-page="languages">
-            Languages
-          </a>
-        </li>
-      </ul>
+      <MainMenu />
     </header>
+  );
+}
+
+function MainMenu() {
+  return (
+    <ul id="top-menu-bar">
+      <li>
+        <a href="#" data-page="home">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="skills">
+          Skills
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="projects">
+          Projects
+        </a>
+      </li>
+      <li>
+        <a href="#" data-page="languages">
+          Languages
+        </a>
+      </li>
+    </ul>
   );
 }
 
