ceacd6e760702a3fb46ec4f4ea2d0cc9c6413a48
Matei Nicolae <nmatei@sdl.com>
2023-05-24 22:05:08 +0300

    import material UI

--- src/menu/MainMenu.tsx
+++ src/menu/MainMenu.tsx
@@ -1,5 +1,7 @@
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import { Page } from "../main/models";
 import "./menu.css";
+import ToggleButton from "@mui/material/ToggleButton";
 
 type Props = {
   active: Page;
@@ -11,6 +13,36 @@ type Actions = {
 type MenuElement = { text: string; name: Page };
 
 export function MainMenu(props: Props & Actions) {
+  const active = props.active;
+  const elements: MenuElement[] = [
+    { text: "Home", name: "home" },
+    { text: "Todos", name: "todos" },
+    { text: "Teams", name: "teams" },
+    { text: "Languages", name: "languages" }
+  ];
+  return (
+    <ToggleButtonGroup
+      value={active}
+      exclusive
+      onChange={(e, active: Page) => {
+        props.setActive(active);
+      }}
+      aria-label="text alignment"
+    >
+      {elements.map(element => (
+        <ToggleButton key={element.name} value={element.name}>
+          {element.text}
+        </ToggleButton>
+      ))}
+      {/* <ToggleButton value="home">Home</ToggleButton>
+      <ToggleButton value="todos">Todos</ToggleButton>
+      <ToggleButton value="teams">Teams</ToggleButton>
+      <ToggleButton value="languages">Languages</ToggleButton> */}
+    </ToggleButtonGroup>
+  );
+}
+
+export function MainMenu_old(props: Props & Actions) {
   const active = props.active;
   const elements: MenuElement[] = [
     { text: "Home", name: "home" },
