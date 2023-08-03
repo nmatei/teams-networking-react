6cadc274a48aa53f5920010d33d1bf06d766e849
Matei Nicolae <nmatei@sdl.com>
2023-08-03 18:20:41 +0300

    add TeamsTable component

--- src/main/components.tsx
+++ src/main/components.tsx
@@ -1,3 +1,18 @@
+import { TeamsTable } from "../teams/TeamsTable";
+
 export function ContentWrapper() {
-  return <div id="main">TODO table...</div>;
+  return (
+    <div id="main">
+      <div className="tbar">
+        <button id="removeSelected">✖ Remove selected</button>
+        <div className="tfill"></div>
+        <input type="search" name="search" id="searchTeams" placeholder="Search..." />
+        <label htmlFor="searchTeams">🔎</label>
+      </div>
+
+      <TeamsTable loading={false} />
+      <br />
+      <TeamsTable loading={true} />
+    </div>
+  );
 }
