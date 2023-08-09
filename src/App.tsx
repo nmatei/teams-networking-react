614aaaf74a37a2313786ffe8aff542b7ad7cfd79
Matei Nicolae <nmatei@sdl.com>
2023-08-09 20:27:28 +0300

    first layout with simple components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/network-team-icon.png";
+import "./App.css";
+import "./style.css";
 
-function App() {
+function App_old() {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +11,42 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+function AppHeader() {
+  return (
+    <header>
+      <div id="header-wrapper">
+        <img src={logo} alt="" width="100" height="100" />
+        <div>
+          <h1>Teams Networking</h1>
+          <h2>CRUD operations example (Create, Read, Update, Delete)</h2>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function ContentWrapper() {
+  return <section id="main">...</section>;
+}
+
+function AppFooter() {
+  return <footer>📃 API Docs | 👨‍💻 Source Code</footer>;
+}
+
+function App() {
+  return (
+    <div>
+      <AppHeader />
+      <ContentWrapper />
+      <AppFooter />
     </div>
   );
 }
