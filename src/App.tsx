e46a84e8e6031e1c322e3d0e8935700b84fa2123
Matei Nicolae <nmatei@sdl.com>
2023-08-09 20:49:03 +0300

    cleanup css and old App

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
 import React from "react";
 import logo from "./images/network-team-icon.png";
-import "./App.css";
 import "./style.css";
-
-function App_old() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 function AppHeader() {
   return (
     <header>
       <div id="header-wrapper">
-        <img src={logo} alt="" width="100" height="100" />
+        <img src={logo} className="App-logo" alt="" width="100" height="100" />
         <div>
           <h1>Teams Networking</h1>
           <h2>CRUD operations example (Create, Read, Update, Delete)</h2>
