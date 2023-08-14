015c67c925fc86ffc7a1ca8eb49be43c1b3d980b
Matei Nicolae <nmatei@sdl.com>
2023-08-14 21:18:25 +0300

    intergrate todos app

--- src/main/components.tsx
+++ src/main/components.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { TeamsTableWrapper } from "../teams/TeamsTable";
 import { Page } from "./models";
+import { TodosApp } from "../todos/components";
 
 export function TeamsPage() {
   const [search, setSearch] = useState("");
@@ -31,10 +32,6 @@ export function HomePage() {
   return <div>Home...</div>;
 }
 
-export function TodosPage() {
-  return <div>Todos...</div>;
-}
-
 type Props = {
   activePage: Page;
 };
@@ -49,7 +46,7 @@ function getActivePage(activePage: Page) {
     case "home":
       return <HomePage />;
     case "todos":
-      return <TodosPage />;
+      return <TodosApp />;
     case "teams":
       return <TeamsPage />;
     default: {
