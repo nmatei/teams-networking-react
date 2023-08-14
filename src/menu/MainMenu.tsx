f167351bb5437a82bc3f6168937bf7cc4518f770
Matei Nicolae <nmatei@sdl.com>
2023-08-14 18:44:43 +0300

    add Page type for useState

--- src/menu/MainMenu.tsx
+++ src/menu/MainMenu.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./menu.css";
 
+type Page = "home" | "todos" | "teams";
+
 export function MainMenu() {
   //const active: string = "teams";
-  const [active, setActive] = useState("home");
+  const [active, setActive] = useState<Page>("home");
   return (
     <ul id="top-menu-bar">
       <li>
