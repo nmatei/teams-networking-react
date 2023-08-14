f25bce6250ebdf04d8a748791f5d5121f67947f6
Matei Nicolae <nmatei@sdl.com>
2023-08-14 18:29:43 +0300

    set active class on click

--- src/menu/MainMenu.tsx
+++ src/menu/MainMenu.tsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
 import "./menu.css";
 
 export function MainMenu() {
-  const active: string = "teams";
+  //const active: string = "teams";
+  const [active, setActive] = useState("home");
   return (
     <ul id="top-menu-bar">
       <li>
-        <a href="#home" className={active === "home" ? "active" : ""}>
+        <a
+          href="#home"
+          className={active === "home" ? "active" : ""}
+          onClick={() => {
+            setActive("home");
+          }}
+        >
           Home
         </a>
       </li>
       <li>
-        <a href="#skills" className={active === "home" ? "active" : ""}>
+        <a
+          href="#todos"
+          className={active === "todos" ? "active" : ""}
+          onClick={() => {
+            setActive("todos");
+          }}
+        >
           Todos
         </a>
       </li>
       <li>
-        <a href="#projects" className={active === "home" ? "active" : ""}>
+        <a
+          href="#teams"
+          className={active === "teams" ? "active" : ""}
+          onClick={() => {
+            setActive("teams");
+          }}
+        >
           Teams
         </a>
       </li>
