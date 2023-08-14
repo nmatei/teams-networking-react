6baa473832c8b8df415688e596880a95643372c9
Matei Nicolae <nmatei@sdl.com>
2023-08-14 18:56:04 +0300

    use array to display multiple elements in menu

--- src/menu/MainMenu.tsx
+++ src/menu/MainMenu.tsx
@@ -4,43 +4,30 @@ import "./menu.css";
 type Page = "home" | "todos" | "teams";
 
 export function MainMenu() {
-  //const active: string = "teams";
+  //const active: string = "teams"; // 48 lines
   const [active, setActive] = useState<Page>("home");
+
+  const elements = [
+    { text: "Home", name: "home" },
+    { text: "Todos", name: "todos" },
+    { text: "Teams", name: "teams" }
+  ];
+
   return (
     <ul id="top-menu-bar">
-      <li>
-        <a
-          href="#home"
-          className={active === "home" ? "active" : ""}
-          onClick={() => {
-            setActive("home");
-          }}
-        >
-          Home
-        </a>
-      </li>
-      <li>
-        <a
-          href="#todos"
-          className={active === "todos" ? "active" : ""}
-          onClick={() => {
-            setActive("todos");
-          }}
-        >
-          Todos
-        </a>
-      </li>
-      <li>
-        <a
-          href="#teams"
-          className={active === "teams" ? "active" : ""}
-          onClick={() => {
-            setActive("teams");
-          }}
-        >
-          Teams
-        </a>
-      </li>
+      {elements.map(element => (
+        <li>
+          <a
+            href={"#" + element.name}
+            className={active === element.name ? "active" : ""}
+            onClick={() => {
+              setActive(element.name as Page); // tmp use 'as Page'
+            }}
+          >
+            {element.text}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
